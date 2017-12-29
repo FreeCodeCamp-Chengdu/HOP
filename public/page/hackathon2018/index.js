695ae9e148f955327f36a84c1527704e3a07ed33
loketa <yyuuqqii@outlook.com>
2017-12-29 17:30:37 +0800

    add banner carousel

--- public/page/hackathon2018/index.js
+++ public/page/hackathon2018/index.js
@@ -3,26 +3,11 @@ require(['jquery', 'EasyWebApp','Swiper'],  function ($, EWA, Swiper) {
     EWA.component(function () {
         this.on('ready',  function () {
             var VM = this;
-            new Swiper('.swiper-guest', {
-                centeredSlides:         true,
-                slidesPerView:          'auto',
-                slideToClickedSlide:    true,
-                grabCursor:             true,
-                loop:                   true,
-                pagination:             '.swiper-pagination',
-                paginationClickable:    true,
-                effect:                 'coverflow',
-                coverflow:              {
-                    rotate: 10,
-                    stretch: 30,
-                    depth: 30,
-                    modifier: 5,
-                    slideShadows : true
-                },
-                onTransitionEnd:        function (swiper) {
-
-                    VM.current = swiper.realIndex;
-                }
+            new Swiper('.banner', {
+                autoplay: 6000,
+                speed: 4000,
+                spaceBetween: 20,
+                loop: true,
             });
         });
     });
