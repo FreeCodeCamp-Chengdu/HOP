ae7638e752da3a1aa64d38e7245a0c49cf79d3e9
wdxia134 <wdxia134@users.noreply.github.com>
2026-03-15 12:00:26 +0800

    fix: resolve issue #32

--- next.config.ts
+++ next.config.ts
@@ -1,50 +1,14 @@
-import { NextConfig } from 'next';
-// @ts-expect-error Upstream has no type package
-import withLess from 'next-with-less';
-import setPWA from 'next-pwa';
-import WP from 'webpack';
-
-const { NODE_ENV, CI } = process.env;
-
-const withPWA = setPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: NODE_ENV === 'development',
-});
-
-const webpack: NextConfig['webpack'] = config => {
-  config.plugins.push(
-    new WP.NormalModuleReplacementPlugin(/^node:/, resource => {
-      resource.request = resource.request.replace(/^node:/, '');
-    }),
-  );
-  return config;
+// Add Tailwind CSS support to Next.js
+module.exports = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    });
+    return config;
+  },
+  experimental: {
+    esmExternals: true,
+  },
 };
-
-const rewrites: NextConfig['rewrites'] = async () => ({
-  beforeFiles: [
-    {
-      source: '/proxy/github.com/:path*',
-      destination: 'https://github.com/:path*',
-    },
-    {
-      source: '/proxy/raw.githubusercontent.com/:path*',
-      destination: 'https://raw.githubusercontent.com/:path*',
-    },
-  ],
-  afterFiles: [],
-  fallback: [],
-});
-
-/** @type {import('next').NextConfig} */
-export default withLess(
-  withPWA({
-    reactStrictMode: true,
-    output: CI ? 'standalone' : undefined,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-    transpilePackages: ['@sentry/browser'],
-    webpack,
-    rewrites,
-  }),
-);
